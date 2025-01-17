2f34e107afbf6983703d853129f6405779f5f03d
Woohyun Jin <neridisoq@gmail.com>
2025-01-18 00:22:56 +0900

    카카오 로그인 구현 일부분만(마무리 해야함)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 
@@ -11,3 +12,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/login")
+async def login():
+    return templates.TemplateResponse("index.html", {"request": request})
